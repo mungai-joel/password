f49d294d9ff5542b80e5ad5f34d6c324e79c2505
mungai-joel <mungaijoel123@gmail.com>
2021-02-15 09:10:12 +0300

    run.py file added

---
+++ run.py
@@ -0,0 +1,35 @@
+from user import User
+
+def create_user(fname,lname,phone,email,password):
+    '''
+    Function to create a  new user
+
+    '''
+    new_user = User(fname,lname,phone,email,password)
+    return new_user
+
+def save_users (user):
+    '''
+    Function to save user
+    '''
+    user.save_user()
+
+def del_user (user):
+    '''
+    Function to delete a user
+    '''
+    user.delete_user()
+
+def find_user(number):
+    '''
+    Function that finds a user by number and returns the user
+
+    '''
+    return User.find_by_number(number)
+
+def check_existing_users(number):
+    '''
+    function that checks if a user exists using number
+    
+    '''
+    return User.user_exist(number)
